4eac7ac5768839eabb786c9ce2b22ea8eb40e093
Fahad Ahmed Chowdhury <fahadchy24@gmail.com>
2023-01-03 20:31:01 +0600

    Added: Roles & Permission relationship

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -7,27 +7,27 @@ use Filament\Tables;
 use Filament\Resources\Form;
 use Filament\Resources\Table;
 use Filament\Resources\Resource;
+use Spatie\Permission\Models\Role;
 use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Components\TextInput;
-use Spatie\Permission\Models\Permission;
 use Filament\Tables\Actions\DeleteAction;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Tables\Actions\DeleteBulkAction;
+use App\Filament\Resources\RoleResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use App\Filament\Resources\PermissionResource\Pages;
-use App\Filament\Resources\PermissionResource\RelationManagers;
-use App\Filament\Resources\PermissionResource\Pages\ManagePermissions;
+use App\Filament\Resources\RoleResource\RelationManagers;
+use App\Filament\Resources\RoleResource\RelationManagers\PermissionsRelationManager;
 
-class PermissionResource extends Resource
+class RoleResource extends Resource
 {
-    protected static ?string $model = Permission::class;
+    protected static ?string $model = Role::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-key';
+    protected static ?string $navigationIcon = 'heroicon-o-lock-closed';
 
     protected static ?string $navigationGroup = 'Admin Management';
-
 
     public static function form(Form $form): Form
     {
@@ -36,8 +36,13 @@ class PermissionResource extends Resource
                 Card::make()
                 ->schema([
                     TextInput::make('name')
-                    ->unique()
-                    ->required()
+                        ->unique(ignoreRecord: true)
+                        ->required(),
+                    Select::make('permissions')
+                        ->multiple()
+                        ->relationship('permissions', 'name')
+                        ->preload()
+                        ->required()
                 ])
             ]);
     }
@@ -47,11 +52,13 @@ class PermissionResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('id')->sortable(),
-                TextColumn::make('name')->sortable()->searchable(),
+                TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
                 TextColumn::make('created_at')
-                ->dateTime('d-M-Y')
-                ->sortable()
-                ->searchable(),
+                    ->dateTime('d-M-Y')
+                    ->sortable()
+                    ->searchable(),
                 TextColumn::make('id')->sortable(),
             ])
             ->filters([
@@ -66,10 +73,19 @@ class PermissionResource extends Resource
             ]);
     }
 
+    public static function getRelations(): array
+    {
+        return [
+            PermissionsRelationManager::class,
+        ];
+    }
+
     public static function getPages(): array
     {
         return [
-            'index' => ManagePermissions::route('/'),
+            'index' => Pages\ListRoles::route('/'),
+            'create' => Pages\CreateRole::route('/create'),
+            'edit' => Pages\EditRole::route('/{record}/edit'),
         ];
     }
 }
