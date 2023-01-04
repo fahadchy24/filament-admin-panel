43a408409a12b26ffc08c5c84d4906eddb5e8ce1
Fahad Ahmed Chowdhury <fahadchy24@gmail.com>
2023-01-04 20:49:29 +0600

    Custom User Menu

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -32,6 +32,8 @@ class UserResource extends Resource
 
     protected static ?string $navigationGroup = 'Admin Management';
 
+    protected static bool $shouldRegisterNavigation = false;
+
     public static function form(Form $form): Form
     {
         return $form
